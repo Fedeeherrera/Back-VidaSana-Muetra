d3e5436366ccc22e527ff77d1fb6e9a6968b5b02
Fedeeherrera <Fedeeherrera2001@gmail.com>
2024-07-17 23:10:52 -0300

    activaciond de CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Cambia esto al origen de tu front-end
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
